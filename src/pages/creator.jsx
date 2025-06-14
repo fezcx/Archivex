ad835bfa780729b5b147cecc466d45253d37e280
fezcx <thiagovfx12@gmail.com>
2025-06-14 16:05:35 +0200

    Add initial serverless functions and basic React pages

---
+++ src/pages/creator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreatorPage = () => {
+  return (
+    <div>
+      <h1>Creator Page</h1>
+      <p>Details about creators will be displayed here.</p>
+    </div>
+  );
+};
+
+export default CreatorPage;
